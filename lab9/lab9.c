322c25ce56973cc02e315c95e10d3c763550d4a4
FYLSunghwan <sunghwan519@hotmail.com>
2020-05-27 13:16:59 +0900

    hw: hw9 completed

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -6,20 +6,20 @@
 ////////////////////////// ASSIGNMENT STATEMENT //////////////////////////
 /* CreateGraph
  * Create a graph with nodes. All input nodes will be positive numbers.
- * 
+ *
  * InsertEdge
  * Insert a edge.
- * 
+ *
  * TopSort
  * Print the graph by topological sort. Sort the smaller number key
  * if same priority.
- * 
+ *
  * MakeNewQueue
  * Create a new queue with the size of X.
- * 
+ *
  * Enqueue
  * Insert a new element at the end of the element in the queue.
- * 
+ *
  * Dequeue
  * Pop the node in the front.
  */
@@ -45,6 +45,14 @@ void TopSort(Graph G);
 Queue MakeNewQueue(int X);
 void Enqueue(Queue Q, int X);
 int Dequeue(Queue Q);
+
+int idx(Graph graph, int x) {
+    int i;
+    for(i=0;i<graph->size;i++) {
+        if(graph->node[i]==x) return i;
+    }
+    return -1;
+}
 //////////////////////////////////////////////////////////////////////////
 
 FILE* fi;
@@ -65,14 +73,23 @@ int main(int argc, char* argv[]) {
     int* nodes = (int*)malloc(sizeof(int)*MAXN);
     Graph graph = CreateGraph(nodes);
 
-    int x, y, res;
+    int x, y, res, read;
+    char inp[1024] = {0, }, tmp;
+    char* ptr = inp;
 
-    while(res = fscanf("%d",&x)) {
+
+    fgets(inp, 1024, fi);
+    ptr = inp;
+    while(sscanf(ptr, "%d%n", &x, &read) == 1) {
         graph->node[graph->size++] = x;
+        ptr += read;
     }
-    fflush(fi);
-    while(res = scanf("%d-%d",&x, &y)) {
+
+    fgets(inp, 1024, fi);
+    ptr = inp;
+    while(sscanf(ptr, "%d%c%d%n", &x, &tmp, &y, &read) == 3) {
         InsertEdge(graph, x, y);
+        ptr += read;
     }
     fprintf(fo, "  ");
     for(int i=0;i<graph->size;i++) {
@@ -89,8 +106,6 @@ int main(int argc, char* argv[]) {
     fprintf(fo,"\n");
     TopSort(graph);
 
-    free(queue->key);
-    free(queue);
     for(int i=0;i<MAXN;i++) {
         free(graph->matrix[i]);
     }
@@ -111,35 +126,60 @@ Graph CreateGraph(int* nodes) {
     for(int i=0;i<MAXN;i++) {
         graph->matrix[i] = (int*)malloc(sizeof(int)*MAXN);
     }
+    return graph;
 }
 
 void InsertEdge(Graph G, int a, int b) {
+    a = idx(G, a);
+    b = idx(G, b);
     G->matrix[a][b] = 1;
-    G->matrix[b][b]++;
 }
 
 void TopSort(Graph G) {
     Queue queue = MakeNewQueue(MAXN);
+    int indegree[MAXN] = {0,};
     for(int i=0;i<G->size;i++) {
-        if(G->matrix[i][i]==0) {
-            Enqueue(Q, i);
+        for(int j=0;j<G->size;j++) {
+            indegree[i] += G->matrix[j][i];
         }
+        if(indegree[i]==0) Enqueue(queue, i);
     }
 
     while(queue->qsize) {
-        int now = Dequeue(Q);
+        int now = Dequeue(queue);
+        fprintf(fo, "%d ", G->node[now]);
         for(int i=0;i<G->size;i++) {
             if(i==now) continue;
-            fprintf(fo, "%d ", now);
             G->matrix[now][i]=0;
-            G->matrix[i][i]--;
-            if(G->matrix[i][i]==0) Enqueue(Q, i);
+            indegree[i]--;
+            if(indegree[i]==0) Enqueue(queue, i);
         }
     }
 
     free(queue->key);
     free(queue);
 }
-Queue MakeNewQueue(int X);
-void Enqueue(Queue Q, int X);
-int Dequeue(Queue Q);
+Queue MakeNewQueue(int X) {
+    Queue queue = (Queue)malloc(sizeof(struct _Queue));
+    queue->key = (int*)malloc(sizeof(int)*MAXN);
+    queue->first = queue->qsize = queue->rear = 0;
+    queue->max_queue_size = MAXN-1;
+    return queue;
+}
+void Enqueue(Queue Q, int X) {
+    if(Q->rear == Q->max_queue_size) {
+        fprintf(fo, "Queue is full.\n");
+        return;
+    }
+    Q->qsize++;
+    Q->key[Q->rear++] = X;
+}
+int Dequeue(Queue Q) {
+    if(Q->qsize==0) {
+        fprintf(fo, "Queue is Empty.\n");
+        return -1;
+    }
+    Q->qsize--;
+    Q->first++;
+    return Q->key[Q->first-1];
+}
